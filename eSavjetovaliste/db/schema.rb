0f99734671e519fb47be05a92503a896cdcbf873
kanitaimamovic <kimamovic1@etf.unsa.ba>
2015-03-15 16:57:15 +0100

    deleting existing model roles
    refreshing database

--- eSavjetovaliste/db/schema.rb
+++ eSavjetovaliste/db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313133746) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "roles", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
