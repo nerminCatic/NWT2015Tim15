e87f2803f27bbf7bf374ef26f81c956796f9c12f
Nermin <ncatic1@etf.unsa.ba>
2015-03-16 23:08:02 +0100

    #added some parts of backend for feedbacks

--- eSavjetovaliste/config/routes.rb
+++ eSavjetovaliste/config/routes.rb
@@ -1,4 +1,6 @@
 ESavjetovaliste::Application.routes.draw do
+  resources :feedbacks
+
   resources :users
 
   resources :roles
--- eSavjetovaliste/db/schema.rb
+++ eSavjetovaliste/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315233709) do
+ActiveRecord::Schema.define(version: 20150316214804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feedbacks", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
