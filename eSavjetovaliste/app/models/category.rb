1cfdc59fc7edd22dfc3133d9e42c75cc56d8720f
Nermin <ncatic1@etf.unsa.ba>
2015-03-21 00:17:21 +0100

    rijesen konflikt (namjerni konflikt da provjerim za ubuduce)

--- eSavjetovaliste/app/models/category.rb
+++ eSavjetovaliste/app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :questions
+	
 	# Required fields
 	validates :name, :description, presence: true
 end
--- eSavjetovaliste/app/models/reservation.rb
+++ eSavjetovaliste/app/models/reservation.rb
@@ -2,7 +2,8 @@ class Reservation < ActiveRecord::Base
   belongs_to :user_doctor_id, class_name: "User"
   belongs_to :user_patient_id, class_name: "User"
   belongs_to :user_receive_id, class_name: "User"
-  
+
   # Required fields
   validates :appointment_date, :status, presence: true
+  
 end
