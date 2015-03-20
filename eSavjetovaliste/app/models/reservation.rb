82ef7754dd76307fd472ed072a1a61413d5a9ae5
Nermin <ncatic1@etf.unsa.ba>
2015-03-21 00:12:14 +0100

    validations for required fields
    Nije htio tortoiseSVN da commita, pa sam ovako promijenio

--- eSavjetovaliste/app/models/reservation.rb
+++ eSavjetovaliste/app/models/reservation.rb
@@ -2,4 +2,7 @@ class Reservation < ActiveRecord::Base
   belongs_to :user_doctor_id, class_name: "User"
   belongs_to :user_patient_id, class_name: "User"
   belongs_to :user_receive_id, class_name: "User"
+  
+  # Required fields
+  validates :appointment_date, :status, presence: true
 end
