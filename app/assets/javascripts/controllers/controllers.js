5a08b253798c204787de3b143d2d1d1c4780f568
Kanita Imamovic <kimamovic1@etf.unsa.ba>
2015-04-26 20:20:54 +0200

    home_admin correction

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -148,14 +148,14 @@ controllers.controller('two_way_control', function ($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.',
-     'age': 1},
+     'age': 2},
     {'name': 'Motorola XOOM™ with Wi-Fi',
      'snippet': 'The Next, Next Generation tablet.',
-     'age': 2},
+     'age': 1},
     {'name': 'MOTOROLA XOOM™',
      'snippet': 'The Next, Next Generation tablet.',
      'age': 3}
   ];
 
-  $scope.orderProp = 'age';
+
 });
