2ecbc02418c6c44ed11f38bf2cd50711322e066a
Kanita Imamovic <kimamovic1@etf.unsa.ba>
2015-04-26 20:07:15 +0200

    example of binding

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -144,8 +144,18 @@ controllers.controller('FeedbackController', ['$scope','Feedback','$location',
         }
 }]);
 // Feedback admin
-controllers.controller('two_way_control', ['$scope', '$location', 'GetFeedback',
-  function($scope, $location, GetFeedback) {
-    GetFeedback.get();
-    
-}]);
+controllers.controller('two_way_control', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3}
+  ];
+
+  $scope.orderProp = 'age';
+});
