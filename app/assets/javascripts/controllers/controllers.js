433a57b4531dbe7a1d038d7894d59ef8ee3739b7
Kanita Imamovic <kimamovic1@etf.unsa.ba>
2015-04-27 00:17:24 +0200

    design for feedback_admin and user_management

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -149,6 +149,16 @@ controllers.controller('FeedbackController', ['$scope','Feedback','$location',
 // Feedback admin
 controllers.controller('FeedbackCtrl', ['$scope', 'GetFeedback', function($scope, GetFeedback) {
   $scope.feedbacks = GetFeedback.all();
+        $scope.checkAll = function () {
+        if ($scope.selectedAll) {
+            $scope.selectedAll = true;
+        } else {
+            $scope.selectedAll = false;
+        }
+        angular.forEach($scope.feedbacks, function (feedback) {
+            feedback.Selected = $scope.selectedAll;
+        });
+    }
 }]);
 
 // User management searching
