3281240bbde803be4f725cb05094006875c14459
Kanita Imamovic <kimamovic1@etf.unsa.ba>
2015-05-04 15:20:28 +0200

    rating added (directive) and feedback on image slider

--- app/assets/javascripts/directives/feedbackDirectives.js
+++ app/assets/javascripts/directives/feedbackDirectives.js
@@ -1,19 +1,44 @@
 var feedback_directives = angular.module('feedback_directives', []);
 
 
-feedback_directives.directive('myDomDirective', function () {
-   
-    return {
-        link: function ($scope, element, attrs) {
-            element.bind('click', function () {
-                element.html('You clicked me!');
-            });
-            element.bind('mouseenter', function () {
-                element.css('background-color', 'yellow');
-            });
-            element.bind('mouseleave', function () {
-                element.css('background-color', 'white');
-            });
-        }
-    };
+feedback_directives.directive('starRating', function() {
+return {
+restrict : 'A',
+template : '<ul class="rating">'
+   + ' <li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
+   + '  <i class="fa fa-star-o"></i>'
+   + ' </li>'
+   + '</ul>',
+scope : {
+ ratingValue : '=',
+ max : '=',
+ onRatingSelected : '&'
+},
+link : function(scope, elem, attrs) {
+ var updateStars = function() {
+  scope.stars = [];
+  for ( var i = 0; i < scope.max; i++) {
+   scope.stars.push({
+    filled : i < scope.ratingValue
+   });
+  }
+ };
+ 
+ scope.toggle = function(index) {
+  scope.ratingValue = index + 1;
+  scope.onRatingSelected({
+   rating : index + 1
   });
+ };
+ 
+ scope.$watch('ratingValue',
+  function(oldVal, newVal) {
+   if (newVal) {
+    updateStars();
+   }
+  }
+ );
+}
+};
+}
+);
