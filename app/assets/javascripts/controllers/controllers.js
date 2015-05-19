e3498ef2a9b39658f149cc2ef22ab357b074d07e
merimaziko <merima.ziko@gmail.com>
2015-05-19 23:00:00 +0200

    historicalchart

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -647,4 +647,44 @@ controllers.controller('ChartsController', ['$scope','GetFeedback', 'GetCategory
             }
     }
 
+    $scope.data_historical = GetQuestion.chart();
+    $scope.options_historical = {
+            chart: {
+                
+                type: 'historicalBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 50
+                },
+                x: function(d){return d.label;},
+                y: function(d){return d.value;},
+                showValues: true,
+                valueFormat: function(d){
+                    return d3.format(',.2f')(d);
+                },
+                transitionDuration: 500,
+                xAxis: {
+                    axisLabel: 'X Axis',
+                 //   tickFormat: function(d) {
+                   //     return d3.time.format('%x')(new Date(d))
+                    //},
+                    //rotateLabels: 50,
+                    showMaxMin: false
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    axisLabelDistance: 35,
+                    tickFormat: function(d){
+                        return d3.format(',.2f')(d);
+                    }
+                }
+
+
+               } 
+
+
+    }
 }]);
